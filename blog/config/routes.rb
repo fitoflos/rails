9af278d48d88e4f4b9614b1cbcbee3b922d78f8e
Adolfo Flos <fito.nefty@gmail.com>
2016-12-12 17:38:43 -0300

    blog ruby on rails
    creacion edicion eliminacion y comentarios tambien se integro seguridad
    para eliminar y para editar

---
+++ blog/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :articles do
+    resources :comments
+  end
+  root 'welcome#index'
+
+  #get 'welcome/index'
+  #root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
